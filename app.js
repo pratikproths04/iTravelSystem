c55b29f03caa69d680bd599b20575a6b90bdf41b
Pratik Pandey <pratikproths04@gmail.com>
2017-02-28 19:41:18 -0800

    added npm libraries

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
 var Yelp = require('yelp');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+//app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//error handling middleware should be loaded after the loading the routes
+if (app.get('env') === 'development') {
+  app.use(errorHandler())
+}
 
 app.get('/search', function (req, res) {
 	var place=req.param("name");
@@ -25,6 +50,6 @@ app.get('/search', function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
-  console.log('iTravelSystem app listening on port 3000!')
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
