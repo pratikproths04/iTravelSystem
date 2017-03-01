b2d2f894f4f86b6c58dd7b142a9be314e3c533ff
Pratik Pandey <pratikproths04@gmail.com>
2017-02-28 16:43:13 -0800

    added yelp api

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var Yelp = require('yelp');
 
 app.get('/search', function (req, res) {
-  var location=req.param("location");
-  console.log("location: "+location);
-  res.send('Processed'+ location);
+	var place=req.param("name");
+	console.log("place: "+place); 
+	var yelp = new Yelp({
+	  consumer_key: '6qSzExrzRp8VLcup-She5A',
+	  consumer_secret: 'h3t8vZyydkeEHg9kaefKhP46nWY',
+	  token: 'geCWnAkZMYFWFR4gVYM-mFYg3Bq-_sR6',
+	  token_secret: 'wXcRTdTwVO7IQnAZMr_8BQNQQeQ',
+	});
+	yelp.search({ term: 'tourism', location : place, radius_filter: '10000', limit: '5' })
+	.then(function (data) {
+	  var jsonParse1=JSON.parse(JSON.stringify(data));
+	  console.log("-------------------"+data.businesses[0].name);
+	  res.send({status: "hello from server", "data":data.businesses[0].name});  
+	});
 });
 
 app.listen(3000, function () {
